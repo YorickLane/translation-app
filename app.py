11238bcffeefdd676b354fc79d1ac6d1765f14d3
yangyu <yangyu@yangyu>
2023-10-20 14:50:25 +0800

    feat: handle the .json file

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+# app.py
 import eventlet
+
 eventlet.monkey_patch(socket=True)
 
-from translate import translate_text, translate_locale_file, create_zip
+from translate import translate_text, translate_file, create_zip
 from flask import Flask, request, render_template, send_from_directory, flash, redirect
 import logging
 from werkzeug.utils import secure_filename
@@ -16,20 +18,21 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 socketio = SocketIO(app)
 
-
-
 # Configuration
-UPLOAD_FOLDER = 'uploads'
-OUTPUT_FOLDER = 'output'
-ALLOWED_EXTENSIONS = {'json', 'js'}
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./universal-team-395508-399550064d46.json"
+UPLOAD_FOLDER = "uploads"
+OUTPUT_FOLDER = "output"
+ALLOWED_EXTENSIONS = {"json", "js"}
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+os.environ[
+    "GOOGLE_APPLICATION_CREDENTIALS"
+] = "./universal-team-395508-399550064d46.json"
 
 # Google Translate Client
 translate_client = translate.Client()
 
 # Cache setup
 LAST_CACHED = datetime.datetime.now()
+
 
 @lru_cache(maxsize=None)
 def get_supported_languages():
@@ -39,64 +42,70 @@ def get_supported_languages():
         get_supported_languages.cache_clear()  # clear cache
         LAST_CACHED = now  # update the timestamp
     languages = translate_client.get_languages()
-    return [{"name": lang['name'], "code": lang['language']} for lang in languages]
+    return [{"name": lang["name"], "code": lang["language"]} for lang in languages]
+
 
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def upload_form():
     return render_template("upload.html", languages=get_supported_languages())
 
-@app.route('/output/<filename>')
+
+@app.route("/output/<filename>")
 def uploaded_file(filename):
     return send_from_directory(OUTPUT_FOLDER, filename)
 
-@app.route('/translate', methods=['POST'])
-def translate_file():
+
+@app.route("/translate", methods=["POST"])
+def translate_file_route():
     # Handle file upload
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect('/')
-    
-    file = request.files['file']
-    
-    if file.filename == '':
-        flash('No selected file')
-        return redirect('/')
-    
+    if "file" not in request.files:
+        flash("No file part")
+        return redirect("/")
+
+    file = request.files["file"]
+
+    if file.filename == "":
+        flash("No selected file")
+        return redirect("/")
+
     if not allowed_file(file.filename):
-        flash('Invalid file type')
-        return redirect('/')
+        flash("Invalid file type")
+        return redirect("/")
 
     filename = secure_filename(file.filename)
-    saved_file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    saved_file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
     file.save(saved_file_path)
 
     # Get target languages
-    target_languages = request.form.getlist('languages')
-    
+    target_languages = request.form.getlist("languages")
+
     if not target_languages:
-        flash('No target languages selected')
-        return redirect('/')
+        flash("No target languages selected")
+        return redirect("/")
 
     output_files = []
     total_languages = len(target_languages)
     for index, target_language in enumerate(target_languages):
         try:
-            output_file_name = translate_locale_file(saved_file_path, target_language)
+            output_file_name = translate_file(
+                saved_file_path, target_language
+            )  # Use translate_file instead of translate_locale_file
             output_files.append(os.path.join(OUTPUT_FOLDER, output_file_name))
-            
+
             # Emitting progress
             progress = (index + 1) / total_languages * 100
-            socketio.emit('progress', {'progress': progress}, namespace='/test')
-            
+            socketio.emit("progress", {"progress": progress}, namespace="/test")
+
             # Simulating some delay for demonstration purposes
             sleep(1)
-            
+
         except Exception as e:
             flash(f"Error translating to {target_language}: {e}")
-            return redirect('/')
+            return redirect("/")
 
     # Create a ZIP archive from translated files
     zip_name = f"translations_{filename}.zip"
@@ -109,15 +118,16 @@ def translate_file():
 
     return render_template("success.html", zip_path="/output/" + zip_name)
 
-@socketio.on('connect', namespace='/test')
+
+@socketio.on("connect", namespace="/test")
 def test_connect():
-    print('Client connected')
+    print("Client connected")
 
-@socketio.on('disconnect', namespace='/test')
+
+@socketio.on("disconnect", namespace="/test")
 def test_disconnect():
-    print('Client disconnected')
+    print("Client disconnected")
 
-    
 
-if __name__ == '__main__':
-  socketio.run(app, debug=True, host='127.0.0.1')
+if __name__ == "__main__":
+    socketio.run(app, debug=True, host="127.0.0.1")
--- translate.py
+++ translate.py
@@ -1,4 +1,6 @@
+# translate.py
 import os
+import json
 import zipfile
 import re
 from google.cloud import translate_v2 as translate
@@ -46,7 +48,6 @@ def translate_text(text, target_language="en"):
 
 def translate_locale_file(source_file_path, target_language="en"):
     """Reads a locale JS file, translates the strings, and writes the translated content to a new file."""
-
     with open(source_file_path, "r", encoding="utf-8") as f:
         content = f.read()
 
@@ -78,9 +79,37 @@ def translate_locale_file(source_file_path, target_language="en"):
     return output_file_name
 
 
+def translate_json_file(source_file_path, target_language="en"):
+    """Reads a JSON file, translates the strings, and writes the translated content to a new file."""
+    with open(source_file_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    translated_data = {}
+    for key, value in data.items():
+        translated_value = translate_text(value.strip(), target_language)
+        translated_data[key] = translated_value
+
+    output_file_name = f"{target_language}.json"
+    output_path = os.path.join("output", output_file_name)
+    with open(output_path, "w", encoding="utf-8") as f:
+        json.dump(translated_data, f, ensure_ascii=False, indent=2)
+
+    return output_file_name
+
+
 def create_zip(files, output_filename):
     """Create a ZIP archive from the given list of files."""
-
     with zipfile.ZipFile(output_filename, "w") as zipf:
         for file in files:
             zipf.write(file, os.path.basename(file))
+
+
+def translate_file(source_file_path, target_language="en"):
+    """Determines the file type and calls the appropriate translation function."""
+    file_extension = source_file_path.split(".")[-1]
+    if file_extension == "json":
+        return translate_json_file(source_file_path, target_language)
+    elif file_extension == "js":
+        return translate_locale_file(source_file_path, target_language)
+    else:
+        raise ValueError("Unsupported file type")
